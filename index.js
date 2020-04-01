7f7e3cbe46229adc7bc370c4967617eac3284c5e
Gilad Shohat <gilad.shohat@gmail.com>
2020-04-01 16:58:57 +0300

    build jest test files

---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const fs = require('fs');
+
+const scaffoldTestFile = () => {
+    if (process.argv.length < 6) {
+        return console.error('invalid arguments - please specify the following arguments: myFileName, myFunction, expectedSuccessResult, expectedErrorResult')
+    };
+    let filePathIndex = 3;
+    console.log('process.arv', process.argv);
+
+    const filePath = `${process.argv[filePathIndex]}.test.js`;
+    const funcName = process.argv[++filePathIndex];
+    const expectedSuccessResult = process.argv[++filePathIndex];
+    const expectedErrorResult = process.argv[++filePathIndex];
+
+    console.log(`scaffolding your test file... :)
+    `);
+
+    const jestSuite =
+        `describe('${funcName}', () => { 
+            beforeAll(async() => { 
+                //todo 
+            }); 
+                
+            test('success', async() => { 
+                const result  = ${funcName}(); 
+                expect(result).toEqual(${expectedSuccessResult}); 
+            }); 
+            
+            test('error', async() => { 
+                try { 
+                    ${funcName}(); 
+                } 
+                catch (err) { 
+                    expect(err).toEqual(${expectedErrorResult}); 
+                } 
+            }); 
+        });`;
+    const fileContent = new Uint8Array(Buffer.from(jestSuite));
+    createFile(filePath, fileContent);
+};
+
+const createFile = (filePath, fileContent) => {
+    fs.writeFile(filePath, fileContent, (err) => {
+        if (err) { return console.error('An error occurred: ', err); }
+        console.log('your file is made!');
+    });
+};
+
+scaffoldTestFile();
---
+++ readme.md
@@ -0,0 +1,53 @@
+# scaffolder
+
+CLI for rapidly building test files 
+
+
+[![Known Vulnerabilities](https://snyk.io/test/npm/scaffolder/badge.svg)](https://snyk.io/test/npm/gforce)
+<a href="https://npmcharts.com/compare/scaffolder?minimal=true"><img src="https://img.shields.io/npm/dm/scaffolder.svg?sanitize=true" alt="Downloads"></a>
+<a href="https://www.npmjs.com/package/scaffolder"><img src="https://img.shields.io/npm/v/scaffolder.svg?sanitize=true" alt="Version"></a>
+<a href="https://www.npmjs.com/package/scaffolder"><img src="https://img.shields.io/npm/l/scaffolder.svg?sanitize=true" alt="License"></a>
+
+## Install
+```
+npm i scaffolder -g
+```
+
+## Usage
+
+#### Create jest test file template
+```js
+//cli
+scaffolder jester myfileName myFuncName myExpectedSuccessResult myExpectedErrorResult 
+```
+```js
+//myFileName.test.js
+describe('myFunc', () => { 
+    beforeAll(async() => { 
+        //todo 
+    }); 
+        
+    test('success', async() => { 
+        const result  = myFunc(); 
+        expect(result).toEqual(myExpectedSuccessResult); 
+    }); 
+    
+    test('error', async() => { 
+        try { 
+            myFunc(); 
+        } 
+        catch (err) { 
+            expect(err).toEqual(myExpectedErrorResult); 
+        } 
+    }); 
+});
+```
+
+
+## License
+
+See [license](LICENSE) (MIT License).
+
+## Authors
+
+- Gilad Shohat <gilad.shohat@gmail.com>
